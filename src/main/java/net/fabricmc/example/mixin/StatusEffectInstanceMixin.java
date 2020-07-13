6cda028ea2348b077ce1995b7bad7231f42dd31a
eatmyvenom <jacobhagan2002@gmail.com>
2020-07-13 15:40:58 -0700

    potions now go brrrr

---
+++ src/main/java/net/fabricmc/example/mixin/StatusEffectInstanceMixin.java
@@ -0,0 +1,24 @@
+package net.fabricmc.example.mixin;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(StatusEffectInstance.class)
+public class StatusEffectInstanceMixin {
+
+    @Shadow private int duration;
+
+    @Inject(method = "upgrade", at = @At("HEAD"), cancellable = true)
+    public void upgradeEffect(StatusEffectInstance that, CallbackInfoReturnable<Boolean> cir) {
+        this.duration += that.getDuration();
+    }
+}
